27db5f2028c0db6aa6ebe9737808426ef59cd294
rakeshyadhav21 <rakeshyadhav329@gmail.com>
2026-09-04 12:52:42 +0530

    first commit

---
+++ maximumElement.js
@@ -0,0 +1,18 @@
+nums = [2,2,1,1,1,2,2]
+
+function maxElement(nums){
+    let max = null
+    let count = 0;
+    
+    for(const num of nums){
+        if(count === 0){
+            max = num
+        }
+        count += (num === max) ? 1 : -1
+    }
+
+
+    return max
+}
+
+console.log(maxElement(nums))
---
+++ twoSum.js
@@ -0,0 +1,30 @@
+const nums = [2,7,11,15]
+const target = 9
+
+function twoSum(nums,target){
+    const solution = []
+
+    for(let i=0;i<nums.length;i++){
+        let num = target-nums[i]
+        if(nums.includes(num)){
+            let index1 = i;
+            let index2 = nums.indexOf(num)
+            if(!solution.includes(index1) && !solution.includes(index2)){
+                solution.push(index1)
+                solution.push(index2)
+            }
+        }
+    }
+
+    return solution.sort((a,b) => a-b)
+}
+
+console.log(twoSum(nums,target))
+
+
+/*
+
+Time complexity : O(n)
+Space complexity : O(1)
+
+*/
